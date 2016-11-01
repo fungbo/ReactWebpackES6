eca5d3ef11bea1befbc2de8422c08be8d790847c
bfeng <fungbo@126.com>
2016-11-01 14:42:25 +0800

    add css

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Hello extends React.Component {
-    render() {
-        return (<h1>Hello</h1>)
-    }
-}
+import Hello from './app/components/hello';
 
 ReactDOM.render(<Hello/>, document.getElementById('app'));
